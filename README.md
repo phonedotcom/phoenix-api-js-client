8bf70593ce07bdda998f25b0ff765d22abb96800
Boris <boris.cuzovic@sentice.com>
2020-07-14 15:34:03 +0200

    adds axios in index.js, adds readme

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# JavaScript Client for api.phone.com
+# Phoenix Api JS Client.
 
+## Description
+Phoenix Api JS Client is a very simple npm package developed by [Phone.com](https://www.phone.com/) that simplifies usage of their API.
+Api documentation can be found [here](https://apidocs.phone.com/).
+
+## Instalation
+Using npm:
+```shell
+$ npm i phoenix-api-js-client
+```
+Note: add  `--save`  if you are using npm < 5.0.0
+
+## Usage
+import the package:
+```shell
+const phoenix = require('phoenix-api-js-client');
+```
+This will provide you access to PhoenixApiClient class. Its constructor receives object with options. Default options are:
+```shell
+options = {
+    client_id: null,
+    handle_rate_limit: true,
+    handle_server_error: 3,
+    session_name: "phoenix-api-js-client-session",
+  };
+```
+
+ - ***client_id*** : string,  ***required*** default null; your api account client id
+
+
+- ***handle_rate_limit***: boolean, default: true; If you send too much requests in certain period of time, it will throw 429 error. Enabling this option, this will wait for the response specified time and it will resend the same request. Logs the message in the console. Disabling this option, it immediately throws the 429 error.
+- ***handle_server_error***: false or unsigned integer, default: 3; Specifies the number of retries if the server responds with 500+ error (500, 501, 502 etc). Logs error in the console.  Disabling this option, it immediately throws the error.
+- ***session_name***: string, default: "phoenix-api-js-client-session"; session name for authenticated users.
+
+After creating this class object, it will give you access to its method. First of them you will probably use are:
+
+|method  | args |description |
+|--|--|--|
+| .get_auth_link |  |  generates sign-in uri. User now can sign in by filling the form, which response will return Bearer token needed for the [OAuth 2.0](https://tools.ietf.org/html/rfc6749). If you use token based authentication, please skip this step. 
+| .load_user  |token (string) **required** |  accepts Bearer token, always starts with Bearer. Sets up the user for the session.
+
+You are now ready to use all of other methods.
+|method  | args |description |
+|--|--|--|
+|sign_out  |  |  sings out the user
+| create_item | uri: string, required; data: object, required | Sends POST request to create the item
+|get_item  | uri: string, required | returns the item specified in the uri.
+| get_list | uri: string, required; limit: unsigned integer, max:500; offset: unsigned integer; global: boolean |  Returns items limited by limit argument, with offset of offset argument and  if global is true looks generaly on the website, if false, looks on the authenticated user account.
+| get_list_all | uri: string, required |  returns all items (specified in the uri) from the account
+| patch_item | uri: string, required; data: object, required | Sends PATCH request to update the item specified in the uri
+| replace_item | uri: string, required; data: object, required | Sends PUT request to update the item specified in the uri
+| download_item | uri: string, required | Downloads the item specified in the uri. 
+| delete_item | uri: string, required | Deletes the item specified in the uri.
+|  |  |  
--- index.js
+++ index.js
@@ -1,65 +1,89 @@
-const USER_STORAGE_KEY = 'mini-user';
+const USER_STORAGE_KEY = "mini-user";
+const axios = require("axios");
 
 class PhoenixApiClient {
-
   user = null;
   extension_id = null;
   options = {
-    'session_name': 'phoenix-api-js-client-session'
+    client_id: null,
+    handle_rate_limit: true,
+    handle_server_error: 3,
+    session_name: "phoenix-api-js-client-session",
   };
 
   constructor(options = {}) {
     Object.assign(this.options, options);
-    const user = sessionStorage.getItem(this.options.session_name);
-    if (user) {
-      user = JSON.parse(user)
-    }
+    let user = sessionStorage.getItem(this.options.session_name);
+    if (user) user = JSON.parse(user);
+
     user && this.set_user(user);
   }
 
   throttleMessage(seconds) {
-    return `Too much requests. It will retry after ${seconds}s`
+    return `Too much requests. It will retry after ${seconds}s`;
   }
 
   handle_rate_limit(err, callback) {
-    const seconds = err.response['@error']['@rateLimit']['Retry-After'] || 1;
+    const seconds = err.data["@error"]["@rateLimit"]["Retry-After"] || 1;
     console.log(this.throttleMessage(seconds));
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        callback().then(resolve).catch(reject);
-      }, seconds * 1000)
-    })
+        callback()
+          .then(resolve)
+          .catch(reject);
+      }, seconds * 1000);
+    });
   }
 
   handle_internal_server_error(err, callback) {
-    console.log('Internal server error. Retrying...');
-    return new Promise((resolve, reject) => { // TODO Add 500ms timeout
-      callback().then(resolve).catch(reject);
-    })
+    console.log(err);
+    console.log("Internal server error. Retrying...");
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        callback()
+          .then(resolve)
+          .catch(reject);
+      }, 500);
+    });
   }
 
   async load_user(token, attempt = 1) {
     try {
       const headers = this.phoenix_auth_headers(token);
-      const response = await ajax('GET', this._phoenix_url('/oauth/access-token', true), null, headers);
-      history.pushState('', document.title, `${window.location.pathname}${window.location.search}`);
+      const response = await axios.get(
+        this._phoenix_url("/oauth/access-token", true),
+        { headers: headers }
+      );
+      history.pushState(
+        "",
+        document.title,
+        `${window.location.pathname}${window.location.search}`
+      );
       this.set_user({
-        'id': response.response['scope_details'][0]['voip_id'],
-        'token': token,
-        'expiration': response.response['expires_at'] ? response.response['expires_at'] * 1000 : null,
+        id: response.data["scope_details"][0]["voip_id"],
+        token: token,
+        expiration: response.data["expires_at"]
+          ? response.data["expires_at"] * 1000
+          : null,
       });
     } catch (err) {
-      if (err.status === 429) {
+      err = err.response;
+      if (err.status === 429 && this.options.handle_rate_limit) {
         await this.handle_rate_limit(err, async () => {
-          await this.load_user(token, attempt)
-        })
+          await this.load_user(token, attempt);
+        });
       }
 
-      if (err.status >= 500 && err.status <=599 && attempt <= 3) {
+      if (
+        err.status >= 500 &&
+        err.status <= 599 &&
+        this.options.handle_server_error &&
+        attempt <= this.options.handle_server_error
+      ) {
         await this.handle_internal_server_error(err, async () => {
           attempt++;
-          return await this.load_user(token, attempt)
-        })
+          return await this.load_user(token, attempt);
+        });
       }
 
       throw err;
@@ -67,226 +91,300 @@ class PhoenixApiClient {
   }
 
   sign_out() {
-    sessionStorage.removeItem(this.session_name);
+    sessionStorage.removeItem(this.options.session_name);
     this.user = null;
     this.extension_id = null;
+    this.options = {
+      client_id: null,
+      handle_rate_limit: true,
+      handle_server_error: 3,
+      session_name: "phoenix-api-js-client-session",
+    };
   }
 
   handle_expired_session() {
-    alert('You session has expired. Please sign in again.');
+    alert("You session has expired. Please sign in again.");
     this.sign_out();
   }
 
   set_user(user) {
     this.user = user;
-    this.set_extension(user.hasOwnProperty('extension') ? user['extension'] : null);
-    if (user['expiration']) {
-      const timeout = user['expiration'] - Date.now() - 10000;
+    this.set_extension(
+      user.hasOwnProperty("extension") ? user["extension"] : null
+    );
+    if (user["expiration"]) {
+      const timeout = user["expiration"] - Date.now() - 10000;
       if (timeout < 0) {
         this.handle_expired_session();
       } else {
-        sessionStorage.setItem(this.session_name, JSON.stringify(user, null, 2));
-        setTimeout(this.handle_expired_session.bind(this), timeout)
+        sessionStorage.setItem(
+          this.options.session_name,
+          JSON.stringify(user, null, 2)
+        );
+        setTimeout(this.handle_expired_session.bind(this), timeout);
       }
     }
   }
 
   set_extension(extension_id) {
-    this.extension_id = extension_id
+    this.extension_id = extension_id;
   }
 
   get_auth_link() {
-    return this._get_auth_link('', true)
+    return this._get_auth_link("", true);
   }
 
   _get_auth_link(redirect_path, is_token) {
-    const client_id = 'cc36cf14-3171-4277-8528-4ab1323ee402';
     const state = Math.floor(Math.random() * 10000000);
     const redirect = `${document.location.protocol}//${document.location.host}${redirect_path}`;
-    return `https://oauth.phone.com/?client_id=${client_id}&response_type=${is_token ? 'token' : 'code'}&scope=account-owner&redirect_uri=${encodeURIComponent(redirect)}&state=${state}`
+    return `https://oauth.phone.com/?client_id=${
+      this.options.client_id
+    }&response_type=${
+      is_token ? "token" : "code"
+    }&scope=account-owner&redirect_uri=${encodeURIComponent(
+      redirect
+    )}&state=${state}`;
   }
 
   _phoenix_url(uri, global) {
-    let url = 'https://api.cit-phone.com/v4';
+    let url = "https://api.cit-phone.com/v4";
     if (!global) {
-      url += `/accounts/${this.user['id']}`;
+      url += `/accounts/${this.user["id"]}`;
     }
-    return `${url}${uri}`
+    return `${url}${uri}`;
   }
 
   phoenix_auth_headers(token) {
-    return (this.user && this.user['token'] || token) ? [
-      {
-        'name': 'Authorization',
-        'value': token ? token : this.user['token']
-      },
-      {
-        "name": "Prefer",
-        "value": "representation=minimal"
-      }
-    ] : []
+    return (this.user && this.user["token"]) || token
+      ? {
+          Authorization: token ? token : this.user["token"],
+          Prefer: "representation=minimal",
+        }
+      : {};
   }
 
   async get_list_all(uri) {
     let all = [];
     let res;
     do {
-      res = await this.get_list(uri, 500, res ? res['offset'] + res['limit'] : 0);
-      all = all.concat(res['items'])
-    } while (res['total'] < all.length);
+      res = await this.get_list(
+        uri,
+        500,
+        res ? res["offset"] + res["limit"] : 0
+      );
+      all = all.concat(res["items"]);
+    } while (res["total"] < all.length);
     return {
-      'items': all,
-      'offset': 0,
-      'total': all.length,
-      'limit': all.length,
-    }
+      items: all,
+      offset: 0,
+      total: all.length,
+      limit: all.length,
+    };
   }
 
-  async get_list(uri, limit, offset, global, attempt=1) {
+  async get_list(uri, limit, offset, global, attempt = 1) {
     try {
       if (limit) {
-        uri += uri.includes('?') ? '&' : '?';
-        uri += 'limit=' + limit;
-        if (offset) uri += '&offset=' + offset;
+        uri += uri.includes("?") ? "&" : "?";
+        uri += "limit=" + limit;
+        if (offset) uri += "&offset=" + offset;
       }
-      const r = await ajax('GET', this._phoenix_url(uri, global), null, this.phoenix_auth_headers());
+      const r = await axios.get(this._phoenix_url(uri, global), {
+        headers: this.phoenix_auth_headers(),
+      });
       return {
-        'items': r.response['items'],
-        'offset': r.response['offset'],
-        'total': r.response['total'],
-        'limit': r.response['limit'],
+        items: r.data["items"],
+        offset: r.data["offset"],
+        total: r.data["total"],
+        limit: r.data["limit"],
       };
     } catch (err) {
-      if (err.status === 429) {
+      err = err;
+      if (err.status === 429 && this.options.handle_rate_limit) {
         return await this.handle_rate_limit(err, async () => {
           return await this.get_list(uri, limit, offset, global, attempt);
-        })
+        });
       }
-      if (err.status >= 500 && err.status <=599 && attempt <= 3) {
+      if (
+        err.status >= 500 &&
+        err.status <= 599 &&
+        this.options.handle_server_error &&
+        attempt <= this.options.handle_server_error
+      ) {
         await this.handle_internal_server_error(err, async () => {
           attempt++;
           return await this.get_list(uri, limit, offset, global, attempt);
-        })
+        });
       }
       throw err;
     }
   }
 
-  async get_item(uri, attempt=1) {
+  async get_item(uri, attempt = 1) {
     try {
-      const item = await ajax('GET', this._phoenix_url(uri), null, this.phoenix_auth_headers());
-      return item.response;
+      const item = await axios.get(this._phoenix_url(uri), {
+        headers: this.phoenix_auth_headers(),
+      });
+      return item.data;
     } catch (err) {
-      if (err.status === 429) {
+      err = err.response;
+      if (err.status === 429 && this.options.handle_rate_limit) {
         return await this.handle_rate_limit(err, async () => {
           return await this.get_item(uri, attempt);
         });
       }
-      if (err.status >= 500 && err.status <=599 && attempt <= 3) {
+      if (
+        err.status >= 500 &&
+        err.status <= 599 &&
+        this.options.handle_server_error &&
+        attempt <= this.options.handle_server_error
+      ) {
         await this.handle_internal_server_error(err, async () => {
           attempt++;
           return await this.get_item(uri, attempt);
-        })
+        });
       }
       throw err;
     }
   }
 
-  async delete_item(uri, attempt=1) {
+  async delete_item(uri, attempt = 1) {
     try {
-      const item = await ajax('DELETE', this._phoenix_url(uri), null, this.phoenix_auth_headers());
-      return item.response;
+      const item = await axios.delete(this._phoenix_url(uri), {
+        headers: this.phoenix_auth_headers(),
+      });
+      return item.data;
     } catch (err) {
-      if (err.status === 429) {
+      err = err.response;
+      if (err.status === 429 && this.options.handle_rate_limit) {
         return await this.handle_rate_limit(err, async () => {
           return await this.delete_item(uri, attempt);
         });
       }
-      if (err.status >= 500 && err.status <=599 && attempt <= 3) {
+      if (
+        err.status >= 500 &&
+        err.status <= 599 &&
+        this.options.handle_server_error &&
+        attempt <= this.options.handle_server_error
+      ) {
         await this.handle_internal_server_error(err, async () => {
           attempt++;
           return await this.delete_item(uri, attempt);
-        })
+        });
       }
       throw err;
     }
   }
 
-  async download_item(uri, attempt=1) {
+  async download_item(uri, attempt = 1) {
     try {
-      const item = await ajax('GET', this._phoenix_url(uri), null, this.phoenix_auth_headers(), null, true);
-      return item.response;
+      const item = await axios.get(this._phoenix_url(uri), {
+        responseType: "blob",
+        timeout: 30000,
+        headers: this.phoenix_auth_headers(),
+      });
+      return item.data;
     } catch (err) {
-      if (err.status === 429) {
+      err = err.response;
+      if (err.status === 429 && this.options.handle_rate_limit) {
         return await this.handle_rate_limit(err, async () => {
           return await this.download_item(uri, attempt);
         });
       }
-      if (err.status >= 500 && err.status <=599 && attempt <= 3) {
+      if (
+        err.status >= 500 &&
+        err.status <= 599 &&
+        this.options.handle_server_error &&
+        attempt <= this.options.handle_server_error
+      ) {
         await this.handle_internal_server_error(err, async () => {
           attempt++;
           return await this.download_item(uri, attempt);
-        })
+        });
       }
       throw err;
     }
   }
 
-  async replace_item(uri, data, attempt=1) {
+  async replace_item(uri, data, attempt = 1) {
     try {
-      const item = await ajax('PUT', this._phoenix_url(uri), data, this.phoenix_auth_headers());
-      return item.response;
+      const item = await axios.put(this._phoenix_url(uri), data, {
+        headers: this.phoenix_auth_headers(),
+      });
+      return item.data;
     } catch (err) {
-      if (err.status === 429) {
+      err = err.response;
+      if (err.status === 429 && this.options.handle_rate_limit) {
         return await this.handle_rate_limit(err, async () => {
           return await this.replace_item(uri, data, attempt);
         });
       }
-      if (err.status >= 500 && err.status <=599 && attempt <= 3) {
+      if (
+        err.status >= 500 &&
+        err.status <= 599 &&
+        this.options.handle_server_error &&
+        attempt <= this.options.handle_server_error
+      ) {
         await this.handle_internal_server_error(err, async () => {
           attempt++;
           return await this.replace_item(uri, data, attempt);
-        })
+        });
       }
       throw err;
     }
   }
 
-  async patch_item(uri, data, attempt=1) {
+  async patch_item(uri, data, attempt = 1) {
     try {
-      const item = await ajax('PATCH', this._phoenix_url(uri), data, this.phoenix_auth_headers());
-      return item.response;
+      const item = await axios.patch(this._phoenix_url(uri), data, {
+        headers: this.phoenix_auth_headers(),
+      });
+      return item.data;
     } catch (err) {
-      if (err.status === 429) {
+      err = err.response;
+      if (err.status === 429 && this.options.handle_rate_limit) {
         return await this.handle_rate_limit(err, async () => {
           return await this.patch_item(uri, data, attempt);
         });
       }
-      if (err.status >= 500 && err.status <=599 && attempt <= 3) {
+      if (
+        err.status >= 500 &&
+        err.status <= 599 &&
+        this.options.handle_server_error &&
+        attempt <= this.options.handle_server_error
+      ) {
         await this.handle_internal_server_error(err, async () => {
           attempt++;
           return await this.patch_item(uri, data, attempt);
-        })
+        });
       }
       throw err;
     }
   }
 
-  async create_item(uri, data, attempt=1) {
+  async create_item(uri, data, attempt = 1) {
     try {
-      const item = await ajax('POST', this._phoenix_url(uri), data, this.phoenix_auth_headers());
-      return item.response;
+      const item = await axios.post(this._phoenix_url(uri), data, {
+        headers: this.phoenix_auth_headers(),
+      });
+      return item.data;
     } catch (err) {
-      if (err.status === 429) {
+      err = err.response;
+      if (err.status === 429 && this.options.handle_rate_limit) {
         return await this.handle_rate_limit(err, async () => {
           return await this.create_item(uri, data, attempt);
         });
       }
-      if (err.status >= 500 && err.status <=599 && attempt <= 3) {
+      if (
+        err.status >= 500 &&
+        err.status <= 599 &&
+        this.options.handle_server_error &&
+        attempt <= this.options.handle_server_error
+      ) {
         await this.handle_internal_server_error(err, async () => {
           attempt++;
           return await this.create_item(uri, data, attempt);
-        })
+        });
       }
       throw err;
     }
